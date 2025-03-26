8b071bdef58e8e02db14bfb8630b4bcd4f93e1db
OdanLafrance <odanlafrance28@gmail.com>
2025-03-26 15:07:26 +0100

    signal

--- assembly_files/assembly_library.h
+++ assembly_files/assembly_library.h
@@ -10,8 +10,3 @@ void handle_signal(int signum, void (*handler)(int));
 timer_t watchdog();
 timer_t watchdog_function (void (*fonction) (union sigval));
 void pet_watchdog(timer_t timer_id, int duration);
-
-// Delay
-unsigned long long int compute_iterations(unsigned long long int delay);
-void add_to_time(struct timespec *t, unsigned int delay, const struct timespec *start);
-void delay_until(const struct timespec *start, unsigned int delay);
--- assembly_files/main.c
+++ assembly_files/main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdatomic.h>
 
 #include "assembly.h"
 #include "assembly.c"
+#include "assembly_library.h"
+#include "assembly_library.c"
 
 assembly_line_t line;
+atomic_int shutdown_flag = 0;
+pthread_t threads[7];
 
 typedef struct {
     side_t side;
@@ -17,12 +22,13 @@ void* install_part(void* arg) {
     struct timespec ts;
     if (clock_gettime(CLOCK_REALTIME, &ts) != 0) {
         perror("clock_gettime failed");
+        free(task);
         return NULL;
     }
 
     sleep_until(&ts, (BELT_PERIOD * task->position + 50));
 
-    while (1) { 
+    while (!shutdown_flag) { 
         if (clock_gettime(CLOCK_REALTIME, &ts) != 0) {
             perror("clock_gettime failed");
             return NULL;
@@ -33,11 +39,12 @@ void* install_part(void* arg) {
         sleep_until(&ts, BELT_PERIOD*(MAX_POSITION-2));
     }
 
+    printf("Thread for position %d | side %d exiting.\n", task->position, task->side);
+    free(task);
     return NULL;
 }
 
 void start_installation_threads() {
-    pthread_t threads[7];
     arm_task_t tasks[7] = {
         {LEFT, 1},
         {LEFT, 2},
@@ -56,6 +63,35 @@ void start_installation_threads() {
     }
 }
 
+sem_t sem;
+
+void* handle_show_stats(void* arg) {
+    while(!shutdown_flag){
+        sem_wait(&sem);
+        print_assembly_stats(line);
+    }
+    return NULL;
+}
+
+void handler(int signum) {
+    if(signum == SIGUSR1) {
+        sem_post(&sem);
+    }
+    else if (signum == SIGINT) { // CTRL+C
+        printf("Signal %d received. Shutting down...\n", signum);
+        shutdown_flag = 1;
+    
+        // attends que les threads se termine
+        for (int i = 0; i < 7; i++) {
+            pthread_join(threads[i], NULL);
+        }
+    
+        shutdown_assembly(line); 
+        free_assembly_line(&line);
+        printf("Signal %d handled. Resources released.\n", signum);
+    }
+}
+
 int main(){
     // setup
     init_assembly_line(&line);
@@ -67,14 +103,20 @@ int main(){
     setup_arm(line, PART_LIGHTS, LEFT, 4);
     setup_arm(line, PART_WINDOWS, RIGHT, 5);
 
+    // setup signal
+    sem_init(&sem, 0, 0);
+
+    pthread_t thread;
+    pthread_create(&thread, NULL, handle_show_stats, NULL);
+    handle_signal(SIGUSR1, handler);
+    handle_signal(SIGINT, handler);
+
     // launch thread
     start_installation_threads();
 
     // run
     run_assembly(line);
 
-    print_assembly_stats(line);
-    shutdown_assembly(line);
-    free_assembly_line(&line);
+    pthread_join(thread, NULL);
     return 0;
 }
